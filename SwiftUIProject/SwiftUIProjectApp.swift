c9aa60489013347f43e03196cb4cd2cc957d4baf
Mikhail Dorofeev <crabgore.gonna.hate@gmail.com>
2021-08-03 12:39:07 +0200

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Mihail on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
